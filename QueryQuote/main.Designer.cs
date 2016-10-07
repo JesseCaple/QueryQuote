778e92833a85f740dc8f5a960ed6546d486886b9
Jesse Caple <thisrhythm@gmail.com>
2016-10-07 11:45:19 -0600

    init

---
+++ QueryQuote/main.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace QueryQuote
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.QuoteifyButton = new System.Windows.Forms.Button();
+            this.DeQuoteifyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // QuoteifyButton
+            // 
+            this.QuoteifyButton.Location = new System.Drawing.Point(12, 12);
+            this.QuoteifyButton.Name = "QuoteifyButton";
+            this.QuoteifyButton.Size = new System.Drawing.Size(75, 23);
+            this.QuoteifyButton.TabIndex = 1;
+            this.QuoteifyButton.Text = "Quoteify";
+            this.QuoteifyButton.UseVisualStyleBackColor = true;
+            this.QuoteifyButton.Click += new System.EventHandler(this.QuoteifyButton_Click);
+            // 
+            // DeQuoteifyButton
+            // 
+            this.DeQuoteifyButton.Location = new System.Drawing.Point(93, 12);
+            this.DeQuoteifyButton.Name = "DeQuoteifyButton";
+            this.DeQuoteifyButton.Size = new System.Drawing.Size(75, 23);
+            this.DeQuoteifyButton.TabIndex = 2;
+            this.DeQuoteifyButton.Text = "DeQuoteify";
+            this.DeQuoteifyButton.UseVisualStyleBackColor = true;
+            this.DeQuoteifyButton.Click += new System.EventHandler(this.DeQuoteifyButton_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(180, 47);
+            this.ControlBox = false;
+            this.Controls.Add(this.DeQuoteifyButton);
+            this.Controls.Add(this.QuoteifyButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button QuoteifyButton;
+        private System.Windows.Forms.Button DeQuoteifyButton;
+    }
+}
+
---
+++ QueryQuote/main.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QueryQuote
+{
+    public partial class Main : Form, IMessageFilter
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        public const int WM_LBUTTONDOWN = 0x0201;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+
+        public Main()
+        {
+            InitializeComponent();
+            Application.AddMessageFilter(this);
+        }
+
+        public bool PreFilterMessage(ref Message m)
+        {
+            if (m.Msg == WM_LBUTTONDOWN && Control.FromHandle(m.HWnd) == this)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+                return true;
+            }
+            return false;
+        }
+
+        private void QuoteifyButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string text = Clipboard.GetText().Replace("\"", "");
+                using (StringWriter writer = new StringWriter())
+                using (StringReader reader = new StringReader(text))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        line = line.TrimEnd();
+                        for (int i = 0; i < line.Length; i++)
+                        {
+                            char c = line[i];
+                            if (!char.IsWhiteSpace(c))
+                            {
+                                writer.WriteLine(line.Substring(0, i) + "\"" + line.Substring(i) + "\"");
+                                break;
+                            }
+                        }
+                    }
+                    text = writer.ToString();
+                }
+                Clipboard.SetText(text);
+            }
+            catch (Exception) { }
+        }
+
+        private void DeQuoteifyButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(Clipboard.GetText().Replace("\"", ""));
+            }
+            catch (Exception) { }
+        }
+    }
+}
